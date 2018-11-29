d22819a0f351500f225917e16f88078b5bf44bf7
nabilahsan <nabilahsan95@gmail.com>
2018-11-29 11:21:53 -0600

    minor rearranging of title
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- login.php
+++ login.php
@@ -14,28 +14,29 @@
 
 <body>
 
-<div class="container-fluid" id="twosided">
-<div class="container" id="left">
-</div>
-
-   <div class="container" id="loginbox">
-   <div class="jumbotron">
-   <h1>MauveHouse Marketing Services</h1>
-   </div>
-      <form class="form-group" action="process.php" method="POST">
-         <h2>Please Log in below</h2>
-         <div class="container" id="logincred">
-            <label id="usr">Username:</label>
-            <input class="form-control" type="text" id="user" name="user" />
-
-            <label id="pwd">Password:</label>
-            <input class="form-control" type="password" id="pass" name="pass" />
-
-            <button type="submit" class="btn btn-lg btn-primary" id="btn" value="Login">Log In</button>
+   <div class="container-fluid" id="twosided">
+      <div class="container" id="left">
+         <div class="jumbotron" id="cname">
+            <h1>MauveHouse Marketing Services</h1>
          </div>
-      </form>
+      </div>
+
+      <div class="container" id="loginbox">
+
+         <form class="form-group" action="process.php" method="POST">
+            <h2>Please Log in below</h2>
+            <div class="container" id="logincred">
+               <label id="usr">Username:</label>
+               <input class="form-control" type="text" id="user" name="user" />
+
+               <label id="pwd">Password:</label>
+               <input class="form-control" type="password" id="pass" name="pass" />
+
+               <button type="submit" class="btn btn-lg btn-primary" id="btn" value="Login">Log In</button>
+            </div>
+         </form>
+      </div>
    </div>
-</div>
 
 </body>
 
